f2ada70a5634097faafa4437bd8c9c78db858a30
Maury03 <diegomqh@gmail.com>
2025-02-06 17:45:58 -0600

    Actividad 3. Extraer lista de palabras

--- main.py
+++ main.py
@@ -2,17 +2,23 @@
 import os
 import time
 from file_reader import open_file, log_results
-from remove_tags import remove_html_tags, log_cleaning_results 
+from remove_tags import remove_html_tags, log_cleaning_results
+from order_words import order_words_file, log_ordering_results
 
 def main():
     # Ruta principal y subcarpeta donde estan los archivos HTML
-    base_directory = r"/Users/athainarada/git/tapioca" # cambiar ruta !!!!
+    base_directory = os.getcwd() # cambiar ruta !!!!
     subfolder = "resources"
     directory = os.path.join(base_directory, subfolder)
+    resources_order_output = "act3_ordered"
+    output_directory = os.path.join(base_directory, resources_order_output)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
     
     # creacion de dos logs separados
     log_file = "a1_matricula.txt"
     log_file_clean = "a2_clean.txt"
+    log_file_order = "a3_matricula.txt"
 
     if not os.path.exists(directory):
         print(f"Error: El directorio '{directory}' no existe.")
@@ -23,6 +29,7 @@ def main():
     total_start_time = time.time()  # Tiempo total de ejecucion
     total_open_time = 0.0  # Tiempo total en abrir los archivos
     total_clean_time = 0.0  # tiempo total de limpieza
+    total_order_time = 0.0 # Tiempo total en ordenar palabras
 
     # Verifica el contenido del directorio
     files_in_directory = os.listdir(directory)
@@ -31,6 +38,7 @@ def main():
     for file_name in sorted(files_in_directory):
         if file_name.endswith(".html"):
             file_path = os.path.join(directory, file_name)
+            output_file = os.path.join(output_directory, file_name)
             print(f"Procesando archivo: {file_path}")
             
             # Medir tiempo de limpieza
@@ -42,6 +50,11 @@ def main():
             start_time = time.time()
             content = open_file(file_path)
             end_time = time.time() 
+
+            # Medir tiempo de ordenado de palabras
+            order_time = order_words_file(file_path, log_file_order, output_file)
+            if order_time is not None:
+                total_order_time += order_time
             
             if content is not None:
                 open_time = end_time - start_time
@@ -49,7 +62,8 @@ def main():
                 results.append({
                     "file": file_name,
                     "time": open_time,
-                    "clean_time": clean_time
+                    "clean_time": clean_time,
+                    "order_time": order_time
                 })
             else:
                 print(f"No se pudo abrir el archivo: {file_path}")
@@ -62,6 +76,9 @@ def main():
 
     log_cleaning_results(log_file_clean, results, total_clean_time, total_execution_time)
     print(f"Resultados de limpieza escritos en {log_file_clean}")
+
+    log_ordering_results(log_file_order, results, total_order_time, total_execution_time)
+    print(f"Resultados de orden escritos en {log_file_order}")
 
 if __name__ == "__main__":
     main()
